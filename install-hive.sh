306f136ddf1939aeca254a0800aa82152944a3d6
Sachin Shankar <sachinshankar2010@gmail.com>
2022-09-02 20:59:34 +0530

    a little more modifications and edge case handling

--- install-hive.sh
+++ install-hive.sh
@@ -1,3 +1,4 @@
+return_to_pwd=$(pwd)
 cd
 if [ -d "hive" ]; then
     echo "Deleting previous hive installation"
@@ -18,12 +19,12 @@ source ~/.bashrc
 
 echo 'HIVE_HOME'
 echo $HIVE_HOME
-echo "Is hive home visible?[y/n]"
+echo "Do you see something like /home/pes1ug20cs999/hive/apache_hive above?[y/n]"
 read answer
 if [ $answer == "y" ]; then
-    echo "You're good to go. Run ./start-hive.sh to start hive"
+    echo "You're good to go. Run ./start-hive.sh to start hive. For now, wait for this process to complete for some post-installation steps."
 else
-    echo "Run source ~/.bashrc and make sure HIVE_HOME is set"
+    echo "Run source ~/.bashrc and make sure HIVE_HOME is set. Once done, run ./start-hive.sh to start hive. For now, wait for this process to complete for some post-installation steps."
 fi
 
 jps_count = $(jps | wc -l)
@@ -38,4 +39,10 @@ if [ $jps_count < 6 ]; then
     jps
     exit 1
 fi
+hdfs dfs -test -d /root/hive/warehouse
+if [ $? == 0 ]; then
+    hdfs dfs -rm -r -f /root*
+fi
 hdfs dfs -mkdir -p /root/hive/warehouse
+
+cd $return_to_pwd
