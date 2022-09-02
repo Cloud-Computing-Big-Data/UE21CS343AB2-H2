9ffd1df7c02d8770465aaa5f127dbb5910fb986c
Sachin Shankar <sachinshankar2010@gmail.com>
2022-09-02 20:42:22 +0530

    made changes to script to make it as error-proof as possible

--- install-hive.sh
+++ install-hive.sh
@@ -13,18 +13,29 @@ mv apache-hive-3.1.3-bin apache_hive
 
 cd
 export HIVE_HOME=$HOME/hive/apache_hive >> ~/.bashrc
+export PATH=$PATH:$HIVE_HOME/bin >> ~/.bashrc
 source ~/.bashrc
-export PATH=$PATH:$HIVE_HOME/bin
-source ~/.bashrc
 
-hadoop version
+echo 'HIVE_HOME'
+echo $HIVE_HOME
+echo "Is hive home visible?[y/n]"
+read answer
+if [ $answer == "y" ]; then
+    echo "You're good to go. Run ./start-hive.sh to start hive"
+else
+    echo "Run source ~/.bashrc and make sure HIVE_HOME is set"
+fi
 
-cd hadoop-3.3.3/sbin/
-./start-all.sh
+jps_count = $(jps | wc -l)
+if [ $jps_count == 1 ]; then
+    echo "Starting Hadoop"
+    $HADOOP_HOME/sbin/start-all.sh
+fi
 
-jps
-
+jps_count = $(jps | wc -l)
+if [ $jps_count < 6 ]; then
+    echo "Hadoop startup failed. Exiting"
+    jps
+    exit 1
+fi
 hdfs dfs -mkdir -p /root/hive/warehouse
-$HIVE_HOME/bin/schematool -initSchema -dbType derby
-cd
-hive
